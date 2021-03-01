8a4cd90e588397d9ca5038b5cdecb2ba0e293474
nummada <madalina.nuta1@gmail.com>
2021-03-01 20:20:43 +0200

    Recipes module

---
+++ Lab 1/recipes.py
@@ -0,0 +1,9 @@
+def read_recipe(type_of_coffee):
+    input_file = open("recipes/" + type_of_coffee + ".txt", "r")
+    input_file.readline()
+    values = input_file.readlines()
+    water = int(values[0][6:])
+    coffee = int(values[1][7:])
+    milk = int(values[2][5:])
+
+    return {"water": water, "coffee": coffee, "milk": milk}
